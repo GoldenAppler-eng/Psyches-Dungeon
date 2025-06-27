906fdbfce329f33c8208bfd094c0b048e824eff8
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-06-27 18:05:03 +1000

    Added Menu template class

--- scripts/menu/start_menu.gd
+++ scripts/menu/start_menu.gd
@@ -3,9 +3,11 @@ extends Menu
 @onready var start_blink_timer : Timer =  %StartBlinkTimer
 @onready var start_label : Label = %start_label
 
-func _process(delta : float) -> void:
-	if not visible:
-		start_blink_timer.stop()
+func enter() -> void:
+	super()
+	
+func exit() -> void:
+	super()
 
 func _on_start_blink_timer_timeout() -> void:
 	start_label.visible = not start_label.visible
