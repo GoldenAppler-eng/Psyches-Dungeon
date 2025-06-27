0b61947b7a99ba7ab9bad3ceab1f043fb868a4a4
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-06-27 20:59:42 +1000

    Added volume controller script

---
+++ scripts/menu/volume_controller.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var select_button : Button
+@export var control_button : Button
+
+@export_enum("Master", "Music", "Sfx") var bus_name : String
+
+var volume_value : int = 100
+
+func _ready() -> void:
+	select_button.pressed.connect(_on_volume_select_button_pressed)
+	select_button.pressed.connect(_on_volume_control_button_pressed)
+
+func _on_volume_select_button_pressed() -> void:
+	control_button.grab_focus()
+
+func _on_volume_control_button_pressed() -> void:
+	var index : int = AudioServer.get_bus_index(bus_name)
+	var value : float = volume_value / 100.0
+	
+	AudioServer.set_bus_volume_db(index, linear_to_db(value))
