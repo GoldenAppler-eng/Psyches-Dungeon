c3da357753b54f099787cb0bc9c9e0619e452fd4
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-07-07 11:03:41 +1000

    Added invincible mode to debug script

--- scripts/DebugMode.gd
+++ scripts/DebugMode.gd
@@ -2,9 +2,14 @@ extends Node
 
 @export var task_handler : TaskHandler
 
+@export var player_invincible : bool = false
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("debug_finish_task"):
 		task_handler.task_finished.emit()
 
 	if Input.is_action_just_pressed("debug_fail_task"):
 		GlobalCardTimer.timeout.emit()
+		
+	if player_invincible:
+		Global.global_player.find_child("DamageableHitbox").monitorable = false
