1ab6acda985eff06b009d0dbecc082a6ac3ca267
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-07-07 11:28:27 +1000

    Added chest-in-range sprite

--- scripts/placeable/chest.gd
+++ scripts/placeable/chest.gd
@@ -11,12 +11,13 @@ const LUCKY_OPEN_CHANCE : float = 0.8
 var _player_nearby := false
 var _opened := false
 
+func _ready() -> void:
+	animated_sprite_2d.frame = 0
+
 func _process(delta: float) -> void:
 	if not _player_nearby or _opened:
 		return
-		
-	animated_sprite_2d.frame = 0	
-		
+
 	if Input.is_action_just_pressed("open_chest"):
 		_open_chest()
 
@@ -24,7 +25,7 @@ func _open_chest() -> void:
 	GlobalSignalBus.chest_opened.emit()
 	
 	_opened = true
-	animated_sprite_2d.frame = 1	
+	animated_sprite_2d.frame = 2	
 	
 	open_sfx.play()
 
@@ -34,7 +35,15 @@ func _open_chest() -> void:
 		enemy_summoner_component.summon_multiple(randi_range(1, 2), true)
 
 func _on_area_2d_player_entered(body: Node2D) -> void:
+	if _opened:
+		return
+	
 	_player_nearby = true
+	animated_sprite_2d.frame = 1
 
 func _on_area_2d_player_exited(body: Node2D) -> void:
+	if _opened:
+		return
+	
 	_player_nearby = false
+	animated_sprite_2d.frame = 0
