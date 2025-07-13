9d45ed1d421f9307ff063cd566c8a4fd225507e2
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-07-13 11:48:29 +1000

    Added activation buffer to trap

--- scripts/placeable/trap.gd
+++ scripts/placeable/trap.gd
@@ -4,23 +4,25 @@ extends RigidBody2D
 @export var damager_hitbox_component : DamagerHitboxComponent
 
 @onready var reset_timer : Timer = %ResetTimer
+@onready var activate_buffer_timer: Timer = %ActivateBufferTimer
 @onready var animated_sprite_2d : AnimatedSprite2D = %AnimatedSprite2D
 
 @onready var activated_sfx : AudioStreamPlayer = %ActivatedSfx
 
-var activated : bool = false
+var _activated : bool = false
+var _is_activating : bool = false
 
 var bodies_entered : int = 0
 
 func _process(delta : float) -> void:
-	if not activated:
+	if not _activated:
 		return
 	
 	damager_hitbox_component.deal_damage_to_area()
 
 func _set_activated(activated : bool) -> void:
-	self.activated = activated
-	animated_sprite_2d.frame = 1 if activated else 0
+	_activated = activated
+	animated_sprite_2d.frame = 2 if activated else 0
 	
 	if activated:
 		activated_sfx.play()
@@ -32,14 +34,24 @@ func _on_activation_area_2d_body_entered(body: Node2D) -> void:
 	reset_timer.stop()
 	bodies_entered += 1
 	
-	if activated:
+	if _activated:
 		return
-		
-	_set_activated(true)	
+	
+	start_activation_process()
 	GlobalSignalBus.trap_activated.emit(body)
+
+func start_activation_process() -> void:
+	activate_buffer_timer.start()
+	_is_activating = true
+	
+	animated_sprite_2d.frame = 1
 
 func _on_activation_area_2d_body_exited(body: Node2D) -> void:
 	bodies_entered -= 1
 	
 	if bodies_entered == 0:
 		reset_timer.start()
+
+func _on_activate_buffer_timer_timeout() -> void:
+	_set_activated(true)	
+	_is_activating = false
