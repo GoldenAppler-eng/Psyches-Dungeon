23f7e8d27e997130cf309ad808322a6b0cbd999d
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-07-16 21:30:20 +1000

    Fixed gold rush enemies not dropping coins

--- scripts/states/EnemyDeadState.gd
+++ scripts/states/EnemyDeadState.gd
@@ -3,18 +3,13 @@ extends State
 @export var summoner_component : SummonerComponent
 @export var enemy : Enemy
 
-var enemy_is_golden : bool = false
-
-func extra_init() -> void:
-	enemy_is_golden = enemy._is_golden
-
 func enter() -> void:
 	super()
 	
 	anim_player.play_animation("death")
-	GlobalSignalBus.enemy_death.emit(enemy_is_golden)
+	GlobalSignalBus.enemy_death.emit(enemy._is_golden)
 	
-	if enemy_is_golden:
+	if enemy._is_golden:
 		summoner_component.summon(true)
 	
 func exit() -> void:
